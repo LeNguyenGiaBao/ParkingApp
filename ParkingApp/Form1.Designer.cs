9c277e71049fddc2d9113d04a8feecde4aef3137
Gia Bảo <lenguyengiabao46@gmail.com>
2021-04-03 10:34:15 +0700

    upload project

---
+++ ParkingApp/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿using System.Windows.Forms;
+
+namespace ParkingApp
+{
+    partial class Form1
+
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnNameApp = new System.Windows.Forms.Panel();
+            this.lbName = new System.Windows.Forms.Label();
+            this.pnMain = new System.Windows.Forms.Panel();
+            this.pic4 = new System.Windows.Forms.PictureBox();
+            this.pic3 = new System.Windows.Forms.PictureBox();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            this.cam4 = new System.Windows.Forms.PictureBox();
+            this.cam2 = new System.Windows.Forms.PictureBox();
+            this.cam3 = new System.Windows.Forms.PictureBox();
+            this.cam1 = new System.Windows.Forms.PictureBox();
+            this.pnNameApp.SuspendLayout();
+            this.pnMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cam4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cam2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cam3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cam1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnNameApp
+            // 
+            this.pnNameApp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(228)))), ((int)(((byte)(242)))), ((int)(((byte)(216)))));
+            this.pnNameApp.Controls.Add(this.lbName);
+            this.pnNameApp.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnNameApp.Location = new System.Drawing.Point(0, 0);
+            this.pnNameApp.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnNameApp.Name = "pnNameApp";
+            this.pnNameApp.Size = new System.Drawing.Size(1345, 56);
+            this.pnNameApp.TabIndex = 13;
+            // 
+            // lbName
+            // 
+            this.lbName.AutoSize = true;
+            this.lbName.Font = new System.Drawing.Font("Calibri", 22.2F, System.Drawing.FontStyle.Bold);
+            this.lbName.ForeColor = System.Drawing.Color.Red;
+            this.lbName.Location = new System.Drawing.Point(347, 7);
+            this.lbName.Name = "lbName";
+            this.lbName.Size = new System.Drawing.Size(763, 46);
+            this.lbName.TabIndex = 0;
+            this.lbName.Text = "HỆ THỐNG QUẢN LÝ BÃI GIỮ XE MOBIPARKING";
+            // 
+            // pnMain
+            // 
+            this.pnMain.Controls.Add(this.pic4);
+            this.pnMain.Controls.Add(this.pic3);
+            this.pnMain.Controls.Add(this.pic2);
+            this.pnMain.Controls.Add(this.pic1);
+            this.pnMain.Controls.Add(this.cam4);
+            this.pnMain.Controls.Add(this.cam2);
+            this.pnMain.Controls.Add(this.cam3);
+            this.pnMain.Controls.Add(this.cam1);
+            this.pnMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnMain.Location = new System.Drawing.Point(0, 56);
+            this.pnMain.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnMain.Name = "pnMain";
+            this.pnMain.Size = new System.Drawing.Size(1345, 810);
+            this.pnMain.TabIndex = 15;
+            // 
+            // pic4
+            // 
+            this.pic4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pic4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic4.Location = new System.Drawing.Point(1031, 267);
+            this.pic4.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.pic4.Name = "pic4";
+            this.pic4.Size = new System.Drawing.Size(300, 240);
+            this.pic4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic4.TabIndex = 25;
+            this.pic4.TabStop = false;
+            // 
+            // pic3
+            // 
+            this.pic3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pic3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic3.Location = new System.Drawing.Point(699, 267);
+            this.pic3.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.pic3.Name = "pic3";
+            this.pic3.Size = new System.Drawing.Size(300, 240);
+            this.pic3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic3.TabIndex = 24;
+            this.pic3.TabStop = false;
+            // 
+            // pic2
+            // 
+            this.pic2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pic2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic2.Location = new System.Drawing.Point(358, 267);
+            this.pic2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(300, 240);
+            this.pic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic2.TabIndex = 23;
+            this.pic2.TabStop = false;
+            // 
+            // pic1
+            // 
+            this.pic1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pic1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic1.Location = new System.Drawing.Point(14, 267);
+            this.pic1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(300, 240);
+            this.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic1.TabIndex = 22;
+            this.pic1.TabStop = false;
+            // 
+            // cam4
+            // 
+            this.cam4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.cam4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cam4.Location = new System.Drawing.Point(1031, 19);
+            this.cam4.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.cam4.Name = "cam4";
+            this.cam4.Size = new System.Drawing.Size(300, 240);
+            this.cam4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cam4.TabIndex = 21;
+            this.cam4.TabStop = false;
+            // 
+            // cam2
+            // 
+            this.cam2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.cam2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cam2.Location = new System.Drawing.Point(358, 19);
+            this.cam2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.cam2.Name = "cam2";
+            this.cam2.Size = new System.Drawing.Size(300, 240);
+            this.cam2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cam2.TabIndex = 20;
+            this.cam2.TabStop = false;
+            // 
+            // cam3
+            // 
+            this.cam3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.cam3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cam3.Location = new System.Drawing.Point(699, 19);
+            this.cam3.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.cam3.Name = "cam3";
+            this.cam3.Size = new System.Drawing.Size(300, 240);
+            this.cam3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cam3.TabIndex = 19;
+            this.cam3.TabStop = false;
+            // 
+            // cam1
+            // 
+            this.cam1.BackColor = System.Drawing.SystemColors.Control;
+            this.cam1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.cam1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cam1.Location = new System.Drawing.Point(14, 19);
+            this.cam1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.cam1.Name = "cam1";
+            this.cam1.Size = new System.Drawing.Size(300, 240);
+            this.cam1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.cam1.TabIndex = 18;
+            this.cam1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1345, 866);
+            this.Controls.Add(this.pnMain);
+            this.Controls.Add(this.pnNameApp);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.MaximumSize = new System.Drawing.Size(1440, 1152);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ParkingLot";
+            this.pnNameApp.ResumeLayout(false);
+            this.pnNameApp.PerformLayout();
+            this.pnMain.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cam4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cam2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cam3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cam1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel pnNameApp;
+        private Panel pnMain;
+        private PictureBox cam2;
+        private PictureBox cam3;
+        private PictureBox cam1;
+        private Label lbName;
+        private PictureBox cam4;
+        private PictureBox pic4;
+        private PictureBox pic3;
+        private PictureBox pic2;
+        private PictureBox pic1;
+    }
+}
---
+++ ParkingApp/Form1.cs
@@ -0,0 +1,139 @@
+﻿using AForge.Video;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ParkingApp
+{
+    public partial class Form1 : Form
+    {
+
+        int _WIDTH_SCREEN = Screen.PrimaryScreen.Bounds.Width;
+        int _HEIGHT_SCREEN = Screen.PrimaryScreen.Bounds.Height;
+
+        string ip1 = @"http://192.168.1.2:4747/video/mjpegfeed?1920x1080";
+        string ip2 = @"http://192.168.1.5:4747/video/mjpegfeed?1920x1080";
+        string ip3 = @"http://192.168.1.7:4747/video/mjpegfeed?1920x1080";
+        //string ip4 = @"http://192.168.1.2:4747/video/mjpegfeed?1920x1080";
+
+
+
+        //MJPEGStream stream;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            setup();
+            start_camera();
+        }
+
+        public void setup()
+        {
+            // form full screen 
+            this.MaximumSize = new Size(_WIDTH_SCREEN, _HEIGHT_SCREEN);
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+
+            this.pnNameApp.Location = new Point(0, 0);
+            this.pnNameApp.Size = new Size(_WIDTH_SCREEN, 70);  // 70 is the size of this picture
+            this.pnNameApp.Dock = DockStyle.Top;
+            
+            this.lbName.Location = new Point((_WIDTH_SCREEN - this.lbName.Width) / 2, (this.pnNameApp.Height - this.lbName.Height) / 2);
+
+            int slit = 20;
+
+            int _picture_size = Convert.ToInt32((_WIDTH_SCREEN - 5 * slit) / 4);
+
+            this.cam1.Size = new Size(_picture_size, _picture_size);
+            this.cam2.Size = new Size(_picture_size, _picture_size);
+            this.cam3.Size = new Size(_picture_size, _picture_size);
+            this.cam4.Size = new Size(_picture_size, _picture_size);
+            this.pic1.Size = new Size(_picture_size, _picture_size);
+            this.pic2.Size = new Size(_picture_size, _picture_size);
+            this.pic3.Size = new Size(_picture_size, _picture_size);
+            this.pic4.Size = new Size(_picture_size, _picture_size);
+
+            this.cam1.Location = new Point(slit, slit);
+            this.cam2.Location = new Point(_picture_size + 2 * slit, slit);
+            this.cam3.Location = new Point(2 * _picture_size + 3 * slit, slit);
+            this.cam4.Location = new Point(3 * _picture_size + 4 * slit, slit);
+
+            this.pic1.Location = new Point(slit, _picture_size + 2 *slit);
+            this.pic2.Location = new Point(_picture_size + 2 * slit, _picture_size + 2 * slit);
+            this.pic3.Location = new Point(2 * _picture_size + 3 * slit, _picture_size + 2 * slit);
+            this.pic4.Location = new Point(3 * _picture_size + 4 * slit, _picture_size + 2 * slit);
+        }
+
+        void LoadCam1()
+        {
+            MJPEGStream stream1 = new MJPEGStream(ip1);
+            stream1.NewFrame += Stream1_NewFrame;
+            stream1.Start();
+        }
+
+        void LoadCam2()
+        {
+            MJPEGStream stream2 = new MJPEGStream(ip2);
+            stream2.NewFrame += Stream2_NewFrame;
+            stream2.Start();
+        }
+
+        void LoadCam3()
+        {
+            MJPEGStream stream3 = new MJPEGStream(ip3);
+            stream3.NewFrame += Stream3_NewFrame;
+            stream3.Start();
+        }
+
+        private void Stream1_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bmp = (Bitmap)eventArgs.Frame.Clone();
+            cam1.Image = bmp;
+        }
+
+        private void Stream2_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bmp = (Bitmap)eventArgs.Frame.Clone();
+            cam2.Image = bmp;
+        }
+
+        private void Stream3_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bmp = (Bitmap)eventArgs.Frame.Clone();
+            cam3.Image = bmp;
+        }
+
+        private void start_camera()
+        {
+            ThreadStart ts1 = new ThreadStart(LoadCam1);
+            Thread t1 = new Thread(ts1);
+            t1.IsBackground = true;
+            t1.Start();
+
+            ThreadStart ts2 = new ThreadStart(LoadCam2);
+            Thread t2 = new Thread(ts2);
+            t2.IsBackground = true;
+            t2.Start();
+
+            ThreadStart ts3 = new ThreadStart(LoadCam3);
+            Thread t3 = new Thread(ts3);
+            t3.IsBackground = true;
+            t3.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ThreadStart ts = new ThreadStart(LoadCam1);
+            Thread t = new Thread(ts);
+            t.IsBackground = true;
+            t.Start();
+        }
+    }
+}
